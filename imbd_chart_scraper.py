0b022b0a42af7ce4af0c669c51b7096e8faf8a2c
aimwps <mwpskinner@gmail.com>
2021-04-07 16:13:27 +0100

    Started restruction of main data grabber

--- imbd_chart_scraper.py
+++ imbd_chart_scraper.py
@@ -17,116 +17,116 @@ def get_genre_urls():
 
 
 
-def get_page_data(input_url):
-    # request url
-    page = requests.get(url=input_url)
-
-    # create a BeautifulSoup object
-    soup = BeautifulSoup(page.content, 'html.parser')
-
-    #page title -- WORKS
-    title = soup.find('title')
-    # data['title'] = title.string
-
-
-    #get rating -- WORKS
-    ratings_container = soup.find_all(class_='lister-item-content')
-    print(len(ratings_container))
-    ratings = []
-    for movie in ratings_container:
-        rating_div = movie.find(class_="inline-block ratings-imdb-rating")
-        if rating_div !=None:
-            rating = float(rating_div.attrs.get("data-value"))
-            ratings.append(rating)
-        else:
-            ratings.append("NaN")
-
-
-    #release-date
-
-
-
-    #get genre --WORKS
-    genre_container = soup.find_all(class_='genre')
-    gr = [title.get_text() for title in genre_container]
-    gr = [title.split('\n') for title in gr]
-    genre = [title[1] for title in gr]
-
-
-
-
-    #get titles -- WORKS
-    chart_list = soup.find_all(class_="lister-item-content")
-    chart_list = []
-    for movie in chart_list:
-        chart_div = movie.find(class_="lister-item-index unbold text-primary").get_text()
-        chart_list.append(int(chart_div))
-
-
-
-
-
-    titles_container = soup.find_all(class_= "lister-item-header")
-    titles = [title.get_text() for title in titles_container ] #loop to getText in days_container
-    titles = [title.split('\n') for title in titles] #split by \n
-    titles_names = [title[2] for title in titles] #split by \n
-
-
-
-
-    #Actors & Directors
-    movie_list = soup.find_all(class_="lister-item-content")
-    actor_links_by_movie = [movie.select('a[href*="/name"]') for movie in movie_list]
-    actor_names_by_movie = [[href_link.get_text() for href_link in movie_list] for movie_list in actor_links_by_movie]
-    directors =[] ## This one for PD
-    actors =[] ## This one for pd
-    for movie in actor_names_by_movie:
-        if len(movie)>0:
-            directors.append(movie[0])
-            actors.append(", ".join(movie[1:]))
-        else:
-            directors.append("NaN")
-            actors.append("NaN")
-
-    ####### duration ########
-
-    dur_movies = soup.find_all(class_='lister-item-content')
-    movie_durs =[] ###this one
-    #print(len(dur_movies))
-    for movie in dur_movies:
-        duration_span = movie.find(class_="runtime")
-        if duration_span != None:
-            get_text = duration_span.get_text()
-            get_int_text = "".join([i for i in get_text if i.isnumeric()])
-            movie_durs.append(int(get_int_text))
-        else:
-            movie_durs.append("NaN")
-
-
-    #filming date -- WORKS
-    film_release = soup.find_all(class_='lister-item-content')
-    release_year = []
-    for movie in film_release:
-        year_div = movie.find(class_='lister-item-year text-muted unbold')
-        if year_div != None and len(year_div.get_text()) > 4:
-            get_ints = [i for i in year_div.get_text() if i.isnumeric()]
-            year = "".join(get_ints[0:4])
-            release_year.append(year)
-        else:
-            release_year.append("NaN")
-
-    data_dict = {"chart_place": chart_list,
-                 "title": titles_names,
-                 "release_year": release_year,
-                 "duration": movie_durs,
-                 "genre": genre,
-                 "ratings": ratings,
-                 "director": directors,
-                 "starring": actors,
-                 }
-    #[chart_list, titles_names, release_year, movie_durs, genre, ratings, directors, actors]
-    print(data_dict)
-    return data_dict
+# def get_page_data(input_url):
+#     # request url
+#     page = requests.get(url=input_url)
+#
+#     # create a BeautifulSoup object
+#     soup = BeautifulSoup(page.content, 'html.parser')
+#
+#     #page title -- WORKS
+#     title = soup.find('title')
+#     # data['title'] = title.string
+#
+#
+#     #get rating -- WORKS
+#     ratings_container = soup.find_all(class_='lister-item-content')
+#     print(len(ratings_container))
+#     ratings = []
+#     for movie in ratings_container:
+#         rating_div = movie.find(class_="inline-block ratings-imdb-rating")
+#         if rating_div !=None:
+#             rating = float(rating_div.attrs.get("data-value"))
+#             ratings.append(rating)
+#         else:
+#             ratings.append("NaN")
+#
+#
+#     #release-date
+#
+#
+#
+#     #get genre --WORKS
+#     genre_container = soup.find_all(class_='genre')
+#     gr = [title.get_text() for title in genre_container]
+#     gr = [title.split('\n') for title in gr]
+#     genre = [title[1] for title in gr]
+#
+#
+#
+#
+#     #get titles -- WORKS
+#     chart_list = soup.find_all(class_="lister-item-content")
+#     chart_list = []
+#     for movie in chart_list:
+#         chart_div = movie.find(class_="lister-item-index unbold text-primary").get_text()
+#         chart_list.append(int(chart_div))
+#
+#
+#
+#
+#
+#     titles_container = soup.find_all(class_= "lister-item-header")
+#     titles = [title.get_text() for title in titles_container ] #loop to getText in days_container
+#     titles = [title.split('\n') for title in titles] #split by \n
+#     titles_names = [title[2] for title in titles] #split by \n
+#
+#
+#
+#
+#     #Actors & Directors
+#     movie_list = soup.find_all(class_="lister-item-content")
+#     actor_links_by_movie = [movie.select('a[href*="/name"]') for movie in movie_list]
+#     actor_names_by_movie = [[href_link.get_text() for href_link in movie_list] for movie_list in actor_links_by_movie]
+#     directors =[] ## This one for PD
+#     actors =[] ## This one for pd
+#     for movie in actor_names_by_movie:
+#         if len(movie)>0:
+#             directors.append(movie[0])
+#             actors.append(", ".join(movie[1:]))
+#         else:
+#             directors.append("NaN")
+#             actors.append("NaN")
+#
+#     ####### duration ########
+#
+#     dur_movies = soup.find_all(class_='lister-item-content')
+#     movie_durs =[] ###this one
+#     #print(len(dur_movies))
+#     for movie in dur_movies:
+#         duration_span = movie.find(class_="runtime")
+#         if duration_span != None:
+#             get_text = duration_span.get_text()
+#             get_int_text = "".join([i for i in get_text if i.isnumeric()])
+#             movie_durs.append(int(get_int_text))
+#         else:
+#             movie_durs.append("NaN")
+#
+#
+#     #filming date -- WORKS
+#     film_release = soup.find_all(class_='lister-item-content')
+#     release_year = []
+#     for movie in film_release:
+#         year_div = movie.find(class_='lister-item-year text-muted unbold')
+#         if year_div != None and len(year_div.get_text()) > 4:
+#             get_ints = [i for i in year_div.get_text() if i.isnumeric()]
+#             year = "".join(get_ints[0:4])
+#             release_year.append(year)
+#         else:
+#             release_year.append("NaN")
+#
+#     data_dict = {"chart_place": chart_list,
+#                  "title": titles_names,
+#                  "release_year": release_year,
+#                  "duration": movie_durs,
+#                  "genre": genre,
+#                  "ratings": ratings,
+#                  "director": directors,
+#                  "starring": actors,
+#                  }
+#     #[chart_list, titles_names, release_year, movie_durs, genre, ratings, directors, actors]
+#     print(data_dict)
+#     return data_dict
 
 def get_all_genres():
     genre_url_dict = get_genre_urls()
