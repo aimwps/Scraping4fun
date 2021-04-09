ad66e2cba2f7bae29a92665a307ba0f4c8b89a35
aimwps <mwpskinner@gmail.com>
2021-04-09 12:45:03 +0100

    Completed the scraper so it finds 1000 movie details in a single run and exports to csv

--- imbd_chart_scraper.py
+++ imbd_chart_scraper.py
@@ -3,6 +3,8 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from dateutil.parser import parse
 import matplotlib.pyplot as plt
+import numpy as np
+import datetime as dt
 
 def get_genre_urls():
     page = requests.get(url="https://www.imdb.com/chart/top/?ref_=nv_mv_250")
@@ -14,11 +16,9 @@ def get_genre_urls():
         url = genre.find('a', href=True)
         genre_key = url.get_text().replace("\n", "").strip()
         url_dict[genre_key] = "https://www.imdb.com" + url['href'] + "&pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=5aab685f-35eb-40f3-95f7-c53f09d542c3&pf_rd_r=BFTF5NMZ7QJX327P95J6&pf_rd_s=right-6&pf_rd_t=15506&pf_rd_i=top&ref_=chttp_gnr_1"
-    for key, value in url_dict.items():
-        print(key + " " + value)
+    # for key, value in url_dict.items():
+    #     print(key + " " + value)
     return url_dict
-
-test = get_genre_urls()
 
 
 
@@ -27,153 +27,96 @@ def get_page_data(input_url):
     page = requests.get(url=input_url)
     soup = BeautifulSoup(page.content, 'html.parser')
     movie_split = soup.find_all(class_='lister-item-content')
-    for movie in movie_split:
-        print(movie)
+
+    # Genres
+    genres = [movie.find('span', class_="genre").get_text().replace("\n", "").strip() for movie in movie_split]
+
+    # Get the rank by convert to an integer the findings of the rank located in the span and class. Take the text exluding the last as it is punctuation.
+    movie_ranks = [int(movie.find('span', class_="lister-item-index unbold text-primary").get_text()[:-1]) for movie in movie_split]
+
+    # Get the title
+    movie_titles = [movie.find('a').get_text() for movie in movie_split]
+
+    # Get release year
+    release_date_unclean = [movie.find('span', class_="lister-item-year text-muted unbold").get_text() for movie in movie_split]
+    release_date = [int("".join([i for i in movie if i.isnumeric()])) for movie in release_date_unclean]
+
+    # get duration
+    movie_durs_unclean =[movie.find(class_="runtime").get_text() for movie in movie_split]
+    movie_durs = [int("".join([i for i in movie if i.isnumeric()])) for movie in movie_durs_unclean]
+
+    movie_rating = [float(movie.find(class_="inline-block ratings-imdb-rating")["data-value"]) for movie in movie_split]
+
+    #Actors & Directors
+    stars_by_movie_unclean = [movie.select('a[href*="/name"]') for movie in movie_split]
+    stars_by_movie = [[href_link.get_text() for href_link in movie_list] for movie_list in stars_by_movie_unclean]
+    directors = [star[0] for star in stars_by_movie]
+    actors =[", ".join([star for star in movie[1:]]) for movie in stars_by_movie]
+
+    # Return data from page
+    data_dict = {"genre": genres,
+                 "genre_rank": movie_ranks,
+                 "title": movie_titles,
+                 "release_year": release_date,
+                 "duration": movie_durs,
+                 "ratings": movie_rating,
+                 "director": directors,
+                 "starring": actors,
+                 }
+    print(f"{genres[0].strip().zfill(50)}: genres:{len(genres)}, movie_ranks:{len(movie_ranks)}, movie_titles:{len(movie_titles)}, release_date:{len(release_date)}, movie_durs:{len(movie_durs)}, movie_rating:{len(movie_rating)},  directors{len(directors)}, actors:{len(actors)}")
+    return data_dict
 
 
-get_page_data(test['Action'])
+
+def data_length_validator(movie_data):
+    invalid_fields = []
+    for field, data in movie_data.items():
+        if len(data)!= 50:
+            invalid_fields.append(field)
+
+    if len(invalid_fields) == 0:
+        return (True, invalid_fields)
+    else:
+        return (False, invalid_fields)
+
+
+def merge_data_dicts(list_of_dictionaries):
+    all_data = {}
+    for dict in list_of_dictionaries:
+        #print(f"working on the dict: {dict}")
+        for key, value in dict.items():
+            #print(f"Current key {key}, current data{value}")
+            if key in all_data.keys():
+                current_data = all_data[key]
+                #print(current_data)
+                combined_data = current_data + value
+                all_data[key] = combined_data
+            else:
+                #print(False)
+                all_data[key] = value
+        #print(all_data)
+    return all_data
+
+def gather_check_combine_web_data():
+    get_urls = get_genre_urls()
+    accepted_genres = []
+    rejected_genres = []
+    for genre, url in get_urls.items():
+        movie_data_dict = get_page_data(url)
+        check_data = data_length_validator(movie_data_dict)
+        if check_data[0] == True:
+            accepted_genres.append(movie_data_dict)
+            print("Accepted Genre")
+        else:
+            rejected_genres.append((genre, check_data[0]))
+            print("**********WARNING******* Rejected a Genre")
+    combined_data = merge_data_dicts(accepted_genres)
+    return combined_data
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-#pd_data = get_all_genres()
-#print(pd_data)
-# ## returns the lists of data for the first 50
-# first_50 = get_page_data("https://www.imdb.com/search/title/?genres=western&genres=Adventure&explore=title_type,genres&ref_=adv_explore_rhs")
-# ## returns the lists of data for the second 50
-# second_50 = get_page_data("https://www.imdb.com/search/title/?genres=western,adventure&start=51&explore=title_type,genres&ref_=adv_nxt")
-#
-# # Combines them both into a dictionary containing all 100
-# data_dict = {'Chart Number': first_50[0]+second_50[0],
-#                     'Title': first_50[1]+second_50[1],
-#                     'Release Date': first_50[2]+second_50[2],
-#                     'Duration(min)': first_50[3]+second_50[3],
-#                     'Genre': first_50[4]+second_50[4],
-#                     'Rating': first_50[5]+second_50[5],
-#
-#                     'Director': first_50[6]+second_50[6],
-#                     'Actors': first_50[7]+second_50[7]}
-
-# creates dataframe from the dictionary
-# df = pd.DataFrame(data_dict)
-# def get_page_data(input_url):
-#     # request url
-#     page = requests.get(url=input_url)
-#
-#     # create a BeautifulSoup object
-#     soup = BeautifulSoup(page.content, 'html.parser')
-#
-#     #page title -- WORKS
-#     title = soup.find('title')
-#     # data['title'] = title.string
-#
-#
-#     #get rating -- WORKS
-#     ratings_container = soup.find_all(class_='lister-item-content')
-#     print(len(ratings_container))
-#     ratings = []
-#     for movie in ratings_container:
-#         rating_div = movie.find(class_="inline-block ratings-imdb-rating")
-#         if rating_div !=None:
-#             rating = float(rating_div.attrs.get("data-value"))
-#             ratings.append(rating)
-#         else:
-#             ratings.append("NaN")
-#
-#
-#     #release-date
-#
-#
-#
-#     #get genre --WORKS
-#     genre_container = soup.find_all(class_='genre')
-#     gr = [title.get_text() for title in genre_container]
-#     gr = [title.split('\n') for title in gr]
-#     genre = [title[1] for title in gr]
-#
-#
-#
-#
-#     #get titles -- WORKS
-#     chart_list = soup.find_all(class_="lister-item-content")
-#     chart_list = []
-#     for movie in chart_list:
-#         chart_div = movie.find(class_="lister-item-index unbold text-primary").get_text()
-#         chart_list.append(int(chart_div))
-#
-#
-#
-#
-#
-#     titles_container = soup.find_all(class_= "lister-item-header")
-#     titles = [title.get_text() for title in titles_container ] #loop to getText in days_container
-#     titles = [title.split('\n') for title in titles] #split by \n
-#     titles_names = [title[2] for title in titles] #split by \n
-#
-#
-#
-#
-#     #Actors & Directors
-#     movie_list = soup.find_all(class_="lister-item-content")
-#     actor_links_by_movie = [movie.select('a[href*="/name"]') for movie in movie_list]
-#     actor_names_by_movie = [[href_link.get_text() for href_link in movie_list] for movie_list in actor_links_by_movie]
-#     directors =[] ## This one for PD
-#     actors =[] ## This one for pd
-#     for movie in actor_names_by_movie:
-#         if len(movie)>0:
-#             directors.append(movie[0])
-#             actors.append(", ".join(movie[1:]))
-#         else:
-#             directors.append("NaN")
-#             actors.append("NaN")
-#
-#     ####### duration ########
-#
-#     dur_movies = soup.find_all(class_='lister-item-content')
-#     movie_durs =[] ###this one
-#     #print(len(dur_movies))
-#     for movie in dur_movies:
-#         duration_span = movie.find(class_="runtime")
-#         if duration_span != None:
-#             get_text = duration_span.get_text()
-#             get_int_text = "".join([i for i in get_text if i.isnumeric()])
-#             movie_durs.append(int(get_int_text))
-#         else:
-#             movie_durs.append("NaN")
-#
-#
-#     #filming date -- WORKS
-#     film_release = soup.find_all(class_='lister-item-content')
-#     release_year = []
-#     for movie in film_release:
-#         year_div = movie.find(class_='lister-item-year text-muted unbold')
-#         if year_div != None and len(year_div.get_text()) > 4:
-#             get_ints = [i for i in year_div.get_text() if i.isnumeric()]
-#             year = "".join(get_ints[0:4])
-#             release_year.append(year)
-#         else:
-#             release_year.append("NaN")
-#
-#     data_dict = {"chart_place": chart_list,
-#                  "title": titles_names,
-#                  "release_year": release_year,
-#                  "duration": movie_durs,
-#                  "genre": genre,
-#                  "ratings": ratings,
-#                  "director": directors,
-#                  "starring": actors,
-#                  }
-#     #[chart_list, titles_names, release_year, movie_durs, genre, ratings, directors, actors]
-#     print(data_dict)
-#     return data_dict
+imdb = pd.DataFrame(gather_check_combine_web_data())
+now = dt.date.today()
+file_name = f"top_50_genre_movies_{now}.csv"
+## Saves file by date scraper was run in the parent directory where scraper is located
+imdb.to_csv(f'../{file_name}', index = False, header=True)
